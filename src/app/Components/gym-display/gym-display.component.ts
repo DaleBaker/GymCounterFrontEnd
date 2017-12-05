fb12e282df29abede88964ac5be4e812f03d60c3
Dale <mightymuddy7@gmail.com>
2017-12-06 01:08:15 +1300

    Changed styling and added a second line for predictions.

--- src/app/Components/gym-display/gym-display.component.ts
+++ src/app/Components/gym-display/gym-display.component.ts
@@ -18,14 +18,14 @@ export class GymDisplayComponent implements OnInit {
   ngOnInit() {
     this.gymAddressesService.activeGym.subscribe(activeGym => this.activeGym = activeGym);
     google.charts.load('current', {packages: ['corechart', 'line']});
-    google.charts.setOnLoadCallback(this.drawGraph);
+    google.charts.setOnLoadCallback(this.drawGraphWithPredictive);
 
   }
 
   drawGraph() {
     const data = new google.visualization.DataTable();
     data.addColumn('string', 'X');
-    data.addColumn('number', 'People');
+    data.addColumn('number', 'Actual Number Of People People');
 
     data.addRows([['9:00', 2], ['10:00', 5], ['11:00', 7], ['12:00', 6], ['13:00', 2], ['14:00', 5]]);
 
@@ -33,8 +33,33 @@ export class GymDisplayComponent implements OnInit {
       title: 'Today\'s gym population',
       chartArea: {width: '90%' , height: '90%'},
       hAxis: {
-        title: 'Time',
-        minValue: 0
+        title: 'Time'
+      },
+      vAxis: {
+        title: 'Number Of People'
+      }
+    };
+
+    const chart = new google.visualization.LineChart(document.getElementById('populations_chart'));
+    chart.draw(data, options);
+  }
+
+  drawGraphWithPredictive() {
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'X');
+    data.addColumn('number', 'Predicted Number Of People');
+    data.addColumn('number', 'Actual Number Of People');
+
+    // ['Time', 'Predicted Number Of People', 'Actual Number Of People'],
+
+    data.addRows([ ['9:00', 2, 2],
+        ['10:00', 5, 4], ['11:00', 7, 7], ['12:00', 6, 6], ['13:00', 4, null], ['14:00', 6, null]]);
+
+    const options = {
+      title: 'Today\'s gym population',
+      chartArea: {width: '70%' , height: '90%'},
+      hAxis: {
+        title: 'Time'
       },
       vAxis: {
         title: 'Number Of People'
