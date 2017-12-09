68bc30d214fd17e9d0e4e8b3e741ec03b22b511b
Dale <mightymuddy7@gmail.com>
2017-12-10 02:19:18 +1300

    restructured the gym object and added the two posts necessary for creating new gym and population objects

--- src/assets/Gym.ts
+++ src/assets/Gym.ts
@@ -2,13 +2,17 @@ export class Gym {
   id: number;
   name: string;
   address: string;
+  city: string;
+  country: string;
   population: string[];
 
-  constructor(id: number, name: string, address: string, population: string[]) {
+
+  constructor(id: number, name: string, address: string, city: string, country: string) {
     this.id = id;
     this.name = name;
     this.address = address;
-    this.population = population;
+    this.city = city;
+    this.country = country;
 
   }
 
